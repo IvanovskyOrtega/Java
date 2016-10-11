628097828485298139f9d0818cd5079e9f264112
Miguel Rojas <miguel.rojas115@hotmail.com>
2016-10-11 16:54:29 -0500

    pero mira ese automata
    todo el archivo cambiado

--- Teoría Computacional/Analizador Léxico (Empleando función recursiva indicando cambio de estados)/AutomataTC/src/fuentes/FuncionDeTransicion.java
+++ Teoría Computacional/Analizador Léxico (Empleando función recursiva indicando cambio de estados)/AutomataTC/src/fuentes/FuncionDeTransicion.java
@@ -2,454 +2,47 @@ package fuentes;
 
 public class FuncionDeTransicion {
     Metodos m = new Metodos();
+    Estados e=new Estados();
     public void cambioDeEstado(String s, int a, int b){
        if(b < s.length()){
        Character c = s.charAt(b);    
        b++;
        switch(a){
            case 0:
-               switch(c){
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':
-                        m.esperar();
-                        System.out.println("q0 ---> q1");
-                        m.esperar();
-                        cambioDeEstado(s,1,b);
-                        break;
-                    case '0':
-                        m.esperar();
-                        System.out.println("q0 ---> q3");
-                        m.esperar();
-                        cambioDeEstado(s,3,b);
-                        break;
-                    case '+':
-                    case '-':
-                        m.esperar();
-                        System.out.println("q0 ---> q2");
-                        m.esperar();
-                        cambioDeEstado(s,2,b);
-                        break;
-                    case 'A':
-                    case 'B':
-                    case 'C':
-                    case 'D':
-                    case 'E':
-                    case 'F':
-                    case 'x':
-                    default:
-                        m.esperar();
-                        System.out.println("q0 ---> M");
-                        System.out.println("Formato inválido o cadena no soportada");
-                        m.esperar();
-                        break;
-               }
+               e.q0(c, s, a, b);
                break;
             case 1:
-                switch(c){
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':
-                    case '0':
-                        m.esperar();
-                        System.out.println("q1 ---> q1");
-                        m.esperar();
-                        cambioDeEstado(s,1,b);
-                        break;
-                    case '.':
-                        m.esperar();
-                        System.out.println("q1 ---> q4");
-                        m.esperar();
-                        cambioDeEstado(s,4,b);
-                        break;
-                    case 'A':
-                    case 'B':
-                    case 'C':
-                    case 'D':
-                    case 'E':
-                    case 'F':
-                    case 'x':   
-                    default:
-                        m.esperar();
-                        System.out.println("q0 ---> M");
-                        System.out.println("Formato inválido o cadena no soportada");
-                        m.esperar();
-                        break;
-                }
+                e.q1(c, s, a, b);
                 break;
             case 2:
-                switch(c){
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':
-                        m.esperar();
-                        System.out.println("q2 ---> q1");
-                        m.esperar();
-                        cambioDeEstado(s,1,b);
-                        break;
-                    case '.':
-                        m.esperar();
-                        System.out.println("q2 ---> q4");
-                        m.esperar();
-                        cambioDeEstado(s,4,b);
-                        break;
-                    case '0':
-                        m.esperar();
-                        System.out.println("q2 ---> q3");
-                        m.esperar();
-                        cambioDeEstado(s,3,b);
-                        break;
-                    case 'A':
-                    case 'B':
-                    case 'C':
-                    case 'D':
-                    case 'E':
-                    case 'F':
-                    case 'x':    
-                    default:
-                        m.esperar();
-                        System.out.println("q2 ---> M");
-                        System.out.println("Formato inválido o cadena no soportada");
-                        m.esperar();
-                        break;
-                }
+                e.q2(c, s, a, b);
                 break;
             case 3:
-                switch(c){
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '0':
-                        m.esperar();
-                        System.out.println("q3 ---> q9");
-                        m.esperar();
-                        cambioDeEstado(s,9,b);
-                        break;
-                    case 'x':
-                        m.esperar();
-                        System.out.println("q3 ---> q10");
-                        m.esperar();
-                        cambioDeEstado(s,10,b);
-                        break;
-                    case '.':
-                        m.esperar();
-                        System.out.println("q3 ---> q4");
-                        m.esperar();
-                        cambioDeEstado(s,4,b);
-                        break;
-                    case 'A':
-                    case 'B':
-                    case 'C':
-                    case 'D':
-                    case 'E':
-                    case 'F':
-                    case '8':
-                    case '9':
-                    default:
-                        m.esperar();
-                        System.out.println("q2 ---> M");
-                        System.out.println("Formato inválido o cadena no soportada");
-                        m.esperar();
-                        break;
-                }
+                e.q3(c, s, a, b);
                 break;
             case 4:
-                switch(c){
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':
-                    case '0':
-                        m.esperar();
-                        System.out.println("q4 ---> q5");
-                        m.esperar();
-                        cambioDeEstado(s,5,b);
-                        break;
-                    case 'A':
-                    case 'B':
-                    case 'C':
-                    case 'D':
-                    case 'E':
-                    case 'F':
-                    case 'x':
-                    case '.':
-                    default: 
-                        m.esperar();
-                        System.out.println("q4 ---> M");
-                        System.out.println("Formato inválido o cadena no soportada");
-                        m.esperar();
-                        break;
-                }
+                e.q4(c, s, a, b);
                 break;
             case 5:
-                switch(c){
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':
-                    case '0':
-                        m.esperar();
-                        System.out.println("q5 ---> q5");
-                        m.esperar();
-                        cambioDeEstado(s,5,b);
-                        break;
-                    case 'E':
-                        m.esperar();
-                        System.out.println("q5 ---> q6");
-                        m.esperar();
-                        cambioDeEstado(s,6,b);
-                        break;
-                    case 'A':
-                    case 'B':
-                    case 'C':
-                    case 'D':
-                    case '.':
-                    case 'F':
-                    case 'x':
-                    default: 
-                        m.esperar();
-                        System.out.println("q5 ---> M");
-                        System.out.println("Formato inválido o cadena no soportada");
-                        m.esperar();
-                        break;
-                }
+                e.q5(c, s, a, b);
                 break;
             case 6:
-                switch(c){
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':
-                    case '0':
-                        m.esperar();
-                        System.out.println("q6 ---> q8");
-                        m.esperar();
-                        cambioDeEstado(s,8,b);
-                        break;
-                    case '+':
-                    case '-':
-                        m.esperar();
-                        System.out.println("q6 ---> q7");
-                        m.esperar();
-                        cambioDeEstado(s,7,b);
-                        break;
-                    case 'A':
-                    case 'B':
-                    case 'C':
-                    case 'D':
-                    case 'E':
-                    case 'F':
-                    case 'x':
-                    case '.':
-                    default: 
-                        m.esperar();
-                        System.out.println("q6 ---> M");
-                        System.out.println("Formato inválido o cadena no soportada");
-                        m.esperar();
-                        break;
-                }
+                e.q6(c, s, a, b);
                 break;
             case 7:
-                switch(c){
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':
-                    case '0':
-                        m.esperar();
-                        System.out.println("q7 ---> q8");
-                        m.esperar();
-                        cambioDeEstado(s,8,b);
-                        break;
-                    case 'A':
-                    case 'B':
-                    case 'C':
-                    case 'D':
-                    case 'E':
-                    case 'F':
-                    case 'x':
-                    case '.':    
-                    default: 
-                        m.esperar();
-                        System.out.println("q7 ---> M");
-                        System.out.println("Formato inválido o cadena no soportada");
-                        m.esperar();
-                        break;
-                }
+                e.q7(c, s, a, b);
                 break;
             case 8:
-                switch(c){
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':
-                    case '0':
-                        m.esperar();
-                        System.out.println("q8 ---> q8");
-                        m.esperar();
-                        cambioDeEstado(s,8,b);
-                        break;
-                    case 'A':
-                    case 'B':
-                    case 'C':
-                    case 'D':
-                    case 'E':
-                    case 'F':
-                    case 'x':
-                    case '.':
-                    default: 
-                        m.esperar();
-                        System.out.println("q8 ---> M");
-                        System.out.println("Formato inválido o cadena no soportada");
-                        m.esperar();
-                        break;
-                }
+                e.q8(c, s, a, b);
                 break;
             case 9:
-                switch(c){
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '0':
-                        m.esperar();
-                        System.out.println("q9 ---> q9");
-                        m.esperar();
-                        cambioDeEstado(s,9,b);
-                        break;
-                    case 'A':
-                    case 'B':
-                    case 'C':
-                    case 'D':
-                    case 'E':
-                    case 'F':
-                    case 'x':
-                    case '.':
-                    case '8':
-                    case '9':
-                    default: 
-                        m.esperar();
-                        System.out.println("q9 ---> M");
-                        System.out.println("Formato inválido o cadena no soportada");
-                        m.esperar();
-                        break;
-                }
+                e.q9(c, s, a, b);
                 break;
             case 10:
-                switch(c){
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':    
-                    case '0':
-                    case 'A':
-                    case 'B':
-                    case 'C':
-                    case 'D':
-                    case 'E':
-                    case 'F':
-                        m.esperar();
-                        System.out.println("q10 ---> q11");
-                        m.esperar();
-                        cambioDeEstado(s,11,b);
-                        break;
-                    case 'x':
-                    case '.':
-                    default: 
-                        m.esperar();
-                        System.out.println("q9 ---> M");
-                        System.out.println("Formato inválido o cadena no soportada");
-                        m.esperar();
-                        break;
-                }
+                e.q10(c, s, a, b);
                 break;
             case 11:
-                switch(c){
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':    
-                    case '0':
-                    case 'A':
-                    case 'B':
-                    case 'C':
-                    case 'D':
-                    case 'E':
-                    case 'F':
-                        m.esperar();
-                        System.out.println("q11 ---> q11");
-                        m.esperar();
-                        cambioDeEstado(s,11,b);
-                        break;
-                    case 'x':
-                    case '.':
-                    default: 
-                        m.esperar();
-                        System.out.println("q11 ---> M");
-                        System.out.println("Formato inválido o cadena no soportada");
-                        m.esperar();
-                        break;
-                }
+                e.q11(c, s, a, b);
                 break;
             default:
                 break;
@@ -480,4 +73,3 @@ public class FuncionDeTransicion {
    }
        
 }
-
