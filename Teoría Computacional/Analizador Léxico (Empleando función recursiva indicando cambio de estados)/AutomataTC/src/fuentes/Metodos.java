39d083fe49b6e0d11be24ba501429683c1a22fbf
Miguel Rojas <miguel.rojas115@hotmail.com>
2016-10-11 16:51:18 -0500

    pero mira ese automata
    movi de lugar el cadena.clear()

--- Teoría Computacional/Analizador Léxico (Empleando función recursiva indicando cambio de estados)/AutomataTC/src/fuentes/Metodos.java
+++ Teoría Computacional/Analizador Léxico (Empleando función recursiva indicando cambio de estados)/AutomataTC/src/fuentes/Metodos.java
@@ -2,12 +2,13 @@ package fuentes;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Metodos{
   public void esperar(){
      try{
-        Thread.sleep(250);
+        Thread.sleep(500);
      }catch(InterruptedException e){}
    }
   public void leerArchivoTxt(String direction){//File direction as an argument
@@ -15,16 +16,31 @@ public class Metodos{
             FuncionDeTransicion funcion = new FuncionDeTransicion();
             BufferedReader bf = new BufferedReader(new FileReader(direction));
             String bfRead;
-            String s1 ;
+            String s1;
+            ArrayList<Character> cadena = new ArrayList<>();
+            Alfabeto a = new Alfabeto();
             int numTokens = 0;
+            int numLineas = 0;
+            System.out.println("Que hongo bro, soy tu automata xdxdxd");
+            System.out.println("Voy a chambear :U");
             while((bfRead = bf.readLine()) != null){
                 StringTokenizer st = new StringTokenizer (bfRead);
+                numLineas++;
                 while (st.hasMoreTokens())
                 {
+                    cadena.clear();
                     s1 = st.nextToken();
                     numTokens++;
-                    System.out.println ("Palabra " + numTokens + " es: " + s1);
-                    funcion.cambioDeEstado(s1, 0, 0);
+                    for(int i=0; i<s1.length();i++){
+                        cadena.add(s1.charAt(i));
+                    }
+                    if(a.numeros.containsAll(cadena)==true){
+                        System.out.println ("Palabra " + numTokens + " es: " + s1 + "\n\tEn la lìnea: "+numLineas);
+                        funcion.cambioDeEstado(s1, 0, 0);
+                    }
+                    else{
+                        System.out.println(s1+" <------ Esto no es para mi :V");
+                    }
                 }
             }
         }catch(Exception e)
